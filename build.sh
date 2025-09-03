f59279bce7d625c83922adc0db83c94a0e7c3ea8
dgb173 <dgb173.fotos@gmail.com>
2025-09-03 06:43:55 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr.bin/env bash
+# exit on error
+set -o errexit
+
+# 1. Instala las dependencias de Python
+pip install -r requirements.txt
+
+# 2. Instala los navegadores de Playwright
+#    Este comando es crucial para que el scraper funcione en la nube.
+playwright install --with-deps
